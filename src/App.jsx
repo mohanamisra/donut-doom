b353ca504f85df549ee27aaacc7e3955d8881f1e
Mohana Misra <iammohanamisra@gmail.com>
2024-06-21 22:11:36 +0530

    second attempt with matter js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import {Engine} from "matter-js/src/module/main.js";
 import './App.css'
 import Ball from './components/Ball/Ball.jsx'
 import Goal from './components/Goal/Goal.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const scene = useRef();
+  const engine = useRef(Engine.create());
   return (
-    <div className="gameContainer">
+    <div ref = {scene}>
       <Ball/>
       <Goal/>
     </div>
