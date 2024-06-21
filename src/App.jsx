0fc502190b98737468137f93d422b6e06a6c36c7
Mohana Misra <iammohanamisra@gmail.com>
2024-06-21 21:47:24 +0530

    Ball created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Ball from './components/Ball/Ball.jsx'
+import Goal from './components/Goal/Goal.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="gameContainer">
       <Ball/>
+      <Goal/>
     </div>
   )
 }
---
+++ src/components/Goal/Goal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Goal = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Goal;
