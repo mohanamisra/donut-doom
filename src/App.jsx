6cd89b4d4d9873c1bcfb89d7adad6f04c5c9b2d3
Mohana Misra <iammohanamisra@gmail.com>
2024-06-21 22:32:22 +0530

    third attempt

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState, useRef } from 'react'
-import {Engine} from "matter-js/src/module/main.js";
+import React, { useState, useRef } from 'react'
 import './App.css'
 import Ball from './components/Ball/Ball.jsx'
 import Goal from './components/Goal/Goal.jsx'
 
 function App() {
-  const scene = useRef();
-  const engine = useRef(Engine.create());
+  // const scene = useRef();
+  // const engine = useRef(Engine.create());
   return (
-    <div ref = {scene}>
-      <Ball/>
-      <Goal/>
-    </div>
+      <div>
+      </div>
   )
 }
 
