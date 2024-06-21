322aba2d610e49a64102dabde4f9a8a810e121ba
Mohana Misra <iammohanamisra@gmail.com>
2024-06-21 23:38:51 +0530

    Learning more p5 + matter js

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import React, { useRef, useEffect } from 'react';
 import p5 from 'p5';
+import Matter from 'matter-js'
 import './App.css'
+
+let Engine = Matter.Engine;
+let World = Matter.World;
+let Bodies = Matter.Bodies;
+
+let engine, box1, world;
 
 function sketch(p) {
     p.setup = function() {
         p.createCanvas(p.windowWidth, p.windowHeight);
-        p.background(0);
-        p.circle(200, 200, 200);
+        engine = Engine.create();
+        world = engine.world;
+        box1 = Bodies.rectangle(0, 0, 80, 80);
+        Engine.run(engine);
+        World.add(world, [box1])
     }
 
     p.windowResized = function() {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
     }
 
+    p.mousePressed = function() {
+        let newBox = Bodies.rectangle(p.mouseX, p.mouseY, 20, 20);
+        World.add(world, [newBox]);
+        p.rect(newBox.position.x, newBox.position.y, 20, 20);
+    }
+
     p.draw = function () {
-        // Drawing code goes here
+        p.background(51);
+        p.rect(box1.position.x, box1.position.y, 80, 80)
     }
 }
 
