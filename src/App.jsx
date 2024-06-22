c22eb6bd4d31babe870548fcf0c2171b3f88ae8f
Mohana Misra <iammohanamisra@gmail.com>
2024-06-22 10:42:10 +0530

    Ball shootable

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,31 @@ import Ball from "./components/Ball.jsx";
 import Ground from "./components/Ground.jsx";
 
 let Engine = Matter.Engine;
-let World = Matter.World;
+let World = Matter.Composite;
 let Bodies = Matter.Bodies;
-let ground, box, ball, world, engine;
+let Body = Matter.Body;
+let Mouse = Matter.Mouse;
+let MouseConstraint = Matter.MouseConstraint;
+let ground, box, ball, world, engine, mCon;
 
 function sketch(p) {
     p.setup = function() {
-        p.createCanvas(p.windowWidth, p.windowHeight);
+        const canvas = p.createCanvas(p.windowWidth, p.windowHeight);
         engine = Engine.create();
         world = engine.world;
 
+        const canvasMouse = Mouse.create(canvas.elt);
+        canvasMouse.pixelRatio = p.pixelDensity();
+        const options = {
+            mouse: canvasMouse,
+        }
+        mCon = MouseConstraint.create(engine, options);
+
         ground = new Ground(p.width/2, p.height - 10, p.width, 20, world);
         box = new Box(300, p.height - 100, 50, 75, world);
-        ball = new Ball(50, p.height - 100, 25, world)
+        ball = new Ball(50, p.height - 25, 25, world)
+
+        World.add(world, mCon)
     }
 
     p.windowResized = function() {
@@ -33,6 +45,7 @@ function sketch(p) {
         box.show(p);
         ball.show(p);
     }
+
 }
 
 function App() {
