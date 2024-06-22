3f5c7b54ec6ebb46e4def640e4713b4f6e56d42e
Mohana Misra <iammohanamisra@gmail.com>
2024-06-23 01:35:32 +0530

    restitution and friction to boundary

--- src/components/Boundary.jsx
+++ src/components/Boundary.jsx
@@ -5,5 +5,7 @@ export default class Boundary extends Box {
         super(x, y, w, h, world);
         this.body.isStatic = true;
         this.color = "rgba(255, 0, 0, 0)";
+        this.body.restitution = 0.85;
+        this.body.friction = 0.05;
     }
 }
