576b5bdd0ea3fee3398183db69865928ef4c1ffc
Mohana Misra <iammohanamisra@gmail.com>
2024-06-26 22:39:31 +0530

    Login functionality working

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,17 @@
 import React, {useEffect, useState} from 'react';
-import Rules from "./Rules.jsx";
 import {auth, provider} from "../config.jsx";
 import {signInWithPopup} from "firebase/auth";
+import {useNavigate} from "react-router-dom";
 
-import genBackground from "../assets/images/generalBackground.webp"
+import donutIcon from "../assets/images/donutIcon.webp"
 import googleIcon from "../assets/images/googleIcon.webp"
 import './Pages.css'
 
 const Login = () => {
+    const navigate = useNavigate();
     const [value, setValue] = useState('');
 
-    const handleClick = () => {
+    const handleLogin = () => {
         signInWithPopup(auth, provider)
             .then((data) => {
                 console.log(data);
@@ -19,21 +20,41 @@ const Login = () => {
             });
     }
 
+    const handleLogout = () => {
+        localStorage.clear();
+        window.location.reload();
+    }
+
+    const handlePlayGame = () => {
+        navigate("/game");
+    }
+
+
     useEffect(() => {
         setValue(localStorage.getItem("email"));
     }, []);
 
     return (
-        <div>
-            {value? <Rules/>
-                :
-            <div className = "login-container">
-                <div className="text-container">
-                    <p>Welcome To Donut Doom!</p>
-                    <button onClick={handleClick}>Login with Google</button>
-                </div>
+        <div className = "login-container">
+            <div className="text-container">
+                <img src={donutIcon} alt = "donut icon" width="100px" className = "donut-icon"/>
+                <p className="heading">Welcome To Donut Doom!</p>
+                <p className="text">You accidentally wandered into a cemetery late at night after
+                    buying a box of those sweet, sweet donuts... <br/><strong>AND WOKE UP THE MONSTERS!</strong></p>
+                <p className="text">Quick! Toss the donuts at them to keep them distracted while you figure out how
+                    to get
+                    out of this sickeningly sweet nightmare!</p>
+                {value ? <>
+                        <button className="play-game-button" onClick={handlePlayGame}>Play Game</button>
+                        <button className="logout-button" onClick={handleLogout}>Logout</button>
+                        </>
+                    :
+                    <button className="login-button" onClick={handleLogin}>Login with Google <img src={googleIcon}
+                                                                                                  width="30px"
+                                                                                                  className="google-icon"/>
+                    </button>
+                }
             </div>
-            }
         </div>
     );
 };
