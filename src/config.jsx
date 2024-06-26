8af0610716b2617584c5161a43efec4c6850a7ac
Mohana Misra <iammohanamisra@gmail.com>
2024-06-26 19:49:07 +0530

    Building login functionality

---
+++ src/config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAIF-6oUBLMa2J2fmk-GQp6-vvuUM1r18g",
+    authDomain: "donutdoomarcade.firebaseapp.com",
+    projectId: "donutdoomarcade",
+    storageBucket: "donutdoomarcade.appspot.com",
+    messagingSenderId: "232090199454",
+    appId: "1:232090199454:web:b19e736f05a249c2dd228f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
