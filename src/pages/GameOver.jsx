7c2c713bcd9bd61d1f8c523337de3422c34fc56f
Mohana Misra <iammohanamisra@gmail.com>
2024-06-27 14:34:53 +0530

    Global leaderboard working

--- src/pages/GameOver.jsx
+++ src/pages/GameOver.jsx
@@ -1,13 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import {useLocation, useNavigate} from 'react-router-dom';
 import {auth, db} from "../config.jsx"
-import {doc, getDoc, updateDoc} from "firebase/firestore";
+import {doc, getDoc, getDocs, updateDoc, query, orderBy, limit, collection} from "firebase/firestore";
 import './Pages.css'
 
 const GameOver = () => {
     const location = useLocation();
     const navigate = useNavigate();
     const [player, setPlayer] = useState({});
+    const [leaderboard, setLeaderboard] = useState([]);
 
     const fetchUserData = async() => {
         auth.onAuthStateChanged(async(user) => {
@@ -32,8 +33,21 @@ const GameOver = () => {
         })
     }
 
+    const fetchLeaderboardData = async() => {
+        const usersRef = collection(db, "users");
+        const q = query(usersRef, orderBy("highscore", "desc"), limit(10));
+        const querySnap = await getDocs(q);
+        const newLeaderboard = [];
+        querySnap.forEach((doc) => {
+            newLeaderboard.push(doc.data());
+        })
+        setLeaderboard(newLeaderboard);
+        console.log(newLeaderboard);
+    }
+
     useEffect(() => {
         fetchUserData();
+        fetchLeaderboardData();
     }, []);
 
     const handlePlayGame = () => {
@@ -42,13 +56,31 @@ const GameOver = () => {
 
     return (
         <div className="game-over-container">
-            <div className="leaderboard-container">
+            <div className="stats-container">
                 <p className="heading">Game Over</p>
                 <div className="text">
-                    <p>{player.username? `${player.username}`: "Loading username"}</p>
-                    <p>{location.state ? `Your Score: ${location.state.score}` : `Can't Access Score`}</p>
-                    <p>{player.highscore ? `Your High Score: ${player.highscore}` : `Loading High Score`}</p>
+                    <p><span className="player-stats">{player.username? `RIP ${player.username}`: "Loading username"}</span></p>
+                    <p><span className = "player-stats">Your Score: </span>{location.state ? `${location.state.score}` : `Can't Access Score`}</p>
+                    <p><span className= "player-stats">Your High Score: </span>{player.highscore ? `${player.highscore}` : `Loading High Score`}</p>
                 </div>
+                {leaderboard.length > 0 ?
+                    <div className="leaderboard-container">
+                        <div className="row">
+                            <div className="col heading">Rank</div>
+                            <div className="col heading">Name</div>
+                            <div className="col heading">Score</div>
+                        </div>
+                        {leaderboard.map((player, index) =>{
+                            return(<div className = "row" key = {index}>
+                                <div className="col">{index + 1}</div>
+                                <div className="col">{player.username}</div>
+                                <div className="col">{player.highscore}</div>
+                            </div>
+                                )
+                            }
+                        )}
+                    </div>
+                    : <p>No Leaderboard to display</p>}
                 <button className="play-game-button" onClick={handlePlayGame}>Play Again</button>
             </div>
         </div>
