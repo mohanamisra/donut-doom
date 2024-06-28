8b834b740b4770c17ec2854fbef0a93983f154e1
Mohana Misra <iammohanamisra@gmail.com>
2024-06-28 13:24:56 +0530

    Attempt at solving sizing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './App.css'
 import {BrowserRouter} from "react-router-dom";
 
 // The BrowserRouter component wraps the main App component and enables App to implement routes.
